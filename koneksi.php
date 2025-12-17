b2404f998b86904a392d0d5a26f2fc4fb13b4686
Ananda Faris Ghazi Ramadhan <164128337+SheraFaris@users.noreply.github.com>
2025-12-17 21:42:27 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+// koneksi.php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "login_multi_level";
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+  die("Koneksi gagal: " . $conn->connect_error);
+}
+$conn->set_charset("utf8mb4");
